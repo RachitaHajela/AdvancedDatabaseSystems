435553ac6ef5e8598e5566c45da3954c0fa19f8e
antoloyola90 <antoloyola90@gmail.com>
2015-12-07 18:32:37 -0500

    Documentation in SiteAccessed with formatting

--- RepCRec/RepCRec/src/nyu/edu/src/store/SiteAccessed.java
+++ RepCRec/RepCRec/src/nyu/edu/src/store/SiteAccessed.java
@@ -2,6 +2,13 @@ package nyu.edu.src.store;
 
 public class SiteAccessed {
 
+    /**
+     * This class is used to keep track of the sites that have been accessed by
+     * a Transaction
+     * 
+     * @author Rachita & Anto
+     */
+
     private Site site;
     private int timeOfAccess;
 
